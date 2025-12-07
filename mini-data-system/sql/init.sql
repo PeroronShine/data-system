9e7fe59a28e6c0526eb030b06beebb83629adaed
PeroronShine <148922733+PeroronShine@users.noreply.github.com>
2025-12-07 14:43:56 +1000

    Update init.sql

--- mini-data-system/sql/init.sql
+++ mini-data-system/sql/init.sql
@@ -2,6 +2,7 @@
 CREATE TABLE IF NOT EXISTS fitness_data (
     id SERIAL PRIMARY KEY,
     timestamp TIMESTAMP DEFAULT NOW(),
+    user_id INTEGER NOT NULL,
     steps INTEGER NOT NULL,
     heart_rate INTEGER NOT NULL,
     calories DECIMAL(5,2) NOT NULL,
